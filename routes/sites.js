0f4687a0ac713af543d863a5fb9b3d598e2ece2f
petro-bishko-sigma <petro.bishko@sigma.software>
2022-01-31 16:24:52 +0200

    TBLO-774: fixes

--- routes/sites.js
+++ routes/sites.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET sites list. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.render('sites', { title: 'Express' });
 });
 
 module.exports = router;
